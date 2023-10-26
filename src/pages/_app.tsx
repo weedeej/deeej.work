14dd41545c47b8886be1cb496f770204ea1acd0f
weedeej <devildjoseph666@gmail.com>
2023-10-26 23:47:15 +0800

    Changed title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,17 @@
 import '@/styles/globals.css'
 import { ScopedCssBaseline } from '@mui/material'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ScopedCssBaseline enableColorScheme={false} sx={{backgroundColor: "unset", userSelect: "none"}}>
-      <Component {...pageProps} />
-    </ScopedCssBaseline>
+    <>
+      <Head>
+        <title>Deeej Portfolio</title>
+      </Head>
+      <ScopedCssBaseline enableColorScheme={false} sx={{ backgroundColor: "unset", userSelect: "none" }}>
+        <Component {...pageProps} />
+      </ScopedCssBaseline>
+    </>
   )
 }
